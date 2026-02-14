874b3abdad35c44033912b6357369fe9ee53c78f
Juan Abaunza <jabaunzagarcia@gmail.com>
2026-02-14 16:06:51 -0600

    Add Tests layer, DateRange tests and ToDo tests

---
+++ CleanArchitecture.ApiTemplate.Tests/Domain/Entities/ToDoTests.cs
@@ -0,0 +1,88 @@
+﻿using CleanArchitecture.ApiTemplate.Domain.Entities;
+using CleanArchitecture.ApiTemplate.Domain.Enums;
+using CleanArchitecture.ApiTemplate.Domain.Exceptions;
+
+namespace CleanArchitecture.ApiTemplate.Tests.Domain.Entities;
+
+[TestClass]
+public class ToDoTests
+{
+    [TestMethod]
+    public void Constructor_NullOrEmptyName_ThrowsBusinessRuleException()
+    {
+        Assert.Throws<BusinessRuleException>(() => new ToDo(null!));
+        Assert.Throws<BusinessRuleException>(() => new ToDo(string.Empty));
+        Assert.Throws<BusinessRuleException>(() => new ToDo("   "));
+    }
+
+    [TestMethod]
+    public void Constructor_ValidParameters_CreatesToDo()
+    {
+        // Arrange
+        var name = "Test ToDo";
+        var description = "This is a test ToDo item.";
+        
+        // Act
+        var toDo = new ToDo(name, description);
+
+        // Assert
+        Assert.AreNotEqual(Guid.Empty, toDo.Id);
+        Assert.AreEqual(name, toDo.Name);
+        Assert.AreEqual(description, toDo.Description);
+        Assert.AreEqual(ToDoStatus.NotStarted, toDo.Status);
+        Assert.IsTrue(toDo.CreatedDate <= DateTime.UtcNow);
+    }
+
+    [TestMethod]
+    public void MarkAsInProgress_ValidTransition_UpdatesStatusAndTimeline()
+    {
+        // Arrange
+        var toDo = new ToDo("Test ToDo");
+        
+        // Act
+        toDo.MarkAsInProgress();
+        
+        // Assert
+        Assert.AreEqual(ToDoStatus.InProgress, toDo.Status);
+        Assert.IsNotNull(toDo.Timeline);
+    }
+
+    [TestMethod]
+    public void MarkAsInProgress_InvalidTransition_ThrowsBusinessRuleException()
+    {
+        // Arrange
+        var toDo = new ToDo("Test ToDo");
+
+        // Act
+        toDo.MarkAsInProgress();
+        
+        // Act & Assert
+        Assert.Throws<BusinessRuleException>(() => toDo.MarkAsInProgress());
+    }
+
+    [TestMethod]
+    public void MarkAsCompleted_ValidTransition_UpdatesStatusAndTimeline()
+    {
+        // Arrange
+        var toDo = new ToDo("Test ToDo");
+        toDo.MarkAsInProgress();
+        
+        // Act
+        toDo.MarkAsCompleted();
+        
+        // Assert
+        Assert.AreEqual(ToDoStatus.Completed, toDo.Status);
+        Assert.IsNotNull(toDo.Timeline);
+        Assert.IsTrue(toDo.Timeline!.EndDate.HasValue);
+    }
+
+    [TestMethod]
+    public void MarkAsCompleted_InvalidTransition_ThrowsBusinessRuleException()
+    {
+        // Arrange
+        var toDo = new ToDo("Test ToDo");
+
+        // Act & Assert
+        Assert.Throws<BusinessRuleException>(() => toDo.MarkAsCompleted());
+    }
+}
---
+++ CleanArchitecture.ApiTemplate.Tests/Domain/ValueObjects/DateRangeTests.cs
@@ -0,0 +1,61 @@
+﻿using CleanArchitecture.ApiTemplate.Domain.Exceptions;
+using CleanArchitecture.ApiTemplate.Domain.ValueObject;
+
+namespace CleanArchitecture.ApiTemplate.Tests.Domain.ValueObjects;
+
+[TestClass]
+public class DateRangeTests
+{
+    [TestMethod]
+    public void Constructor_StartDateAfterEndDate_ShouldThrowBusinessRuleException()
+    {
+        // Arrange
+        var startDate = DateTime.UtcNow;
+        var endDate = startDate.AddDays(-1);
+
+        // Act & Assert
+        Assert.Throws<BusinessRuleException>(() => new DateRange(startDate, endDate));
+    }
+
+    [TestMethod]
+    public void Constructor_ValidDates_CreatesDateRange()
+    {
+        // Arrange
+        var startDate = DateTime.UtcNow;
+        var endDate = startDate.AddDays(1);
+
+        // Act
+        var dateRange = new DateRange(startDate, endDate);
+
+        // Assert
+        Assert.AreEqual(startDate, dateRange.StartDate);
+        Assert.AreEqual(endDate, dateRange.EndDate);
+    }
+
+    [TestMethod]
+    public void Complete_EndDateAlreadySet_ShouldThrowBusinessRuleException()
+    {
+        // Arrange
+        var startDate = DateTime.UtcNow;
+        var endDate = startDate.AddDays(1);
+        var dateRange = new DateRange(startDate, endDate);
+
+        // Act & Assert
+        Assert.Throws<BusinessRuleException>(() => dateRange.Complete());
+    }
+
+    [TestMethod]
+    public void Complete_EndDateNotSet_ShouldSetEndDate()
+    {
+        // Arrange
+        var startDate = DateTime.UtcNow;
+        var dateRange = new DateRange(startDate);
+
+        // Act
+        dateRange.Complete();
+
+        // Assert
+        Assert.IsTrue(dateRange.EndDate.HasValue);
+        Assert.IsTrue(dateRange.EndDate.Value >= startDate);
+    }
+}
